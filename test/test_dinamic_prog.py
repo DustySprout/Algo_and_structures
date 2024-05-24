09253d20c3257a70a61cfef7c41fc2e9e91b0abc
DustySprout <150775517+DustySprout@users.noreply.github.com>
2024-05-24 20:43:10 +0300

    Update and rename DinamicProgTest.py to test_dinamic_prog.py

--- test/test_dinamic_prog.py
+++ test/test_dinamic_prog.py
@@ -1,20 +1,20 @@
-from DinamicProg import *
+from dinamic_prog import *
 import unittest
 
 
 class TestIndianaJones(unittest.TestCase):
     def test_case(self):
-        row_size, col_size, sneaky_way = read_input("DinamicProg.in")
+        row_size, col_size, sneaky_way = read_input("dinamic_prog.in")
         result = ijones_traversal(sneaky_way, rows=row_size, cols=col_size)
-        out_put = read_output("DinamicProg.out")
+        out_put = read_output("dinamic_prog.out")
         self.assertEqual(result, out_put)
 
 
 def read_output(path):
     with open(path, 'r', encoding='utf-8') as file:
-        data = file.read().strip()  # Remove any leading/trailing whitespace
+        data = file.read().strip()
         if not data:
-            raise ValueError("file.out порожній")
+            raise ValueError("dinamic_prog.out порожній")
         try:
             return int(data)
         except ValueError as e:
