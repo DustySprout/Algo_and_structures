be12a5b0ab341ebcfc95be04a6c4802e99a4efa3
DustySprout <150775517+DustySprout@users.noreply.github.com>
2024-05-24 20:50:57 +0300

    Update test_game_server.py

--- test/test_game_server.py
+++ test/test_game_server.py
@@ -1,12 +1,12 @@
 import unittest
 
-from src.GameServer import *
+from src.game_server import *
 
 
 class TestGameServerLatency(unittest.TestCase):
     def test_incomplete_connections_list(self):
-        find_minimum_latency_from_file("gamsrv.in", "gamsrv.out")
-        with open('gamsrv.out', 'r') as file:
+        find_minimum_latency_from_file("test.gamsrv.in", "test.gamsrv.out")
+        with open('test.gamsrv.out', 'r') as file:
             first_line = file.readline()
             numbers = first_line.split()
 
