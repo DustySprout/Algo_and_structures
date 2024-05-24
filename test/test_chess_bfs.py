dfaf104226bc6be2f4c5eb809a6b8067bfae8bfa
DustySprout <150775517+DustySprout@users.noreply.github.com>
2024-05-24 20:46:43 +0300

    Update and rename ChessBFSTests.py to test_chess_bfs.py

--- test/test_chess_bfs.py
+++ test/test_chess_bfs.py
@@ -1,19 +1,19 @@
 import unittest
-from ChessBFS import *
+from chess_bfs import *
 
 
 class TestBFSShortMinesPath(unittest.TestCase):
     def test_normal_case(self):
-        find_shortest_safe_route("./resources/input.txt", "./resources/output.txt")
-        with open('./resources/output.txt', 'r') as file:
+        find_shortest_safe_route("test.input.txt", "test.output.txt")
+        with open('test.output.txt', 'r') as file:
             first_line = file.readline()
             numbers = first_line.split()
 
         self.assertEqual(int(numbers[0]),12)
 
     def test_empty_input(self):
-        find_shortest_safe_route("./resources/input_empty.txt", "./resources/output_empty.txt")
-        with open('./resources/output_empty.txt', 'r') as file:
+        find_shortest_safe_route("test.input.txt", "test.output.txt")
+        with open('test.output.txt', 'r') as file:
             first_line = file.readline()
             numbers = first_line.split()
         self.assertEqual(int(numbers[0]), -1)
